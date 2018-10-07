d55a683a12dc40e5ce1f4e7d5f0479f65637e328
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-10-07 12:23:16 +0200

    First formal definition of state and action + Function to calculates probability in transition table

--- reactive/src/template/ReactiveTemplate.java
+++ reactive/src/template/ReactiveTemplate.java
@@ -1,5 +1,9 @@
 package template;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import logist.simulation.Vehicle;
@@ -13,13 +17,112 @@ import logist.task.TaskDistribution;
 import logist.topology.Topology;
 import logist.topology.Topology.City;
 
+
 public class ReactiveTemplate implements ReactiveBehavior {
 
+	class StateList {
+		
+		private List<State> states; 
+		
+		public StateList(Topology topology) {
+			
+			states = new ArrayList<State>();
+			
+			for (City initialCity : topology.cities()) {
+				states.add(new State(initialCity));
+			}
+			
+			for (City initialCity : topology.cities()) {
+				for (City destinationCity : topology.cities()) {
+					states.add(new State(initialCity, destinationCity));
+				}
+			}		
+		}
+		
+		public List<State> states() {
+			return states;
+		}
+	}
+	
+	class State {
+		
+		public City currentCity = null;
+		public City destinationCity = null;
+		
+		public State(City currentCity) {
+			this(currentCity, null);
+		}
+		
+		public State(City currentCity, City destinationCity) {
+			this.currentCity = currentCity;
+			this.destinationCity = destinationCity;
+		}
+		
+		public boolean hasTask() {
+			return destinationCity != null;
+		}
+	}
+	
+	class AgentAction {
+		
+		public City moveCity = null;
+		
+		public AgentAction() {
+			this(null);
+		}
+		
+		public AgentAction(City moveCity) {
+			this.moveCity = moveCity;
+		}
+		
+		public boolean isPickup() {
+			return moveCity == null;
+		}
+	}
+	
+	class AgentActionList {
+		
+		private List<AgentAction> actions; 
+		
+		public AgentActionList(Topology topology) {
+			
+			actions = new ArrayList<AgentAction>();
+			
+			actions.add(new AgentAction());
+			
+			for (City initialCity : topology.cities()) {
+				actions.add(new AgentAction(initialCity));
+			}
+		}
+		
+		public List<AgentAction> actions() {
+			return actions;
+		}
+	}
+		
+	class Decision {
+		public final State state = null;
+		public final Action action = null;
+	}
+	
+	class Transition {
+		public final State initialState = null;
+		public final State finalState = null;
+		public final Action action = null;
+	}
+	
 	private Random random;
 	private double pPickup;
-	private int numActions;
 	private Agent myAgent;
+	private int numActions;
+	
+	
+//	private Map<State, Double> V = new HashMap<State, Double>();
+	private Map<Decision, Double> R = new HashMap<Decision, Double>();
+	private Map<Transition, Double> T = new HashMap<Transition, Double>();
 
+	private List<Double> V = new ArrayList<Double>();
+	
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
 
@@ -51,5 +154,31 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		numActions++;
 		
 		return action;
+	}
+	
+	public double initialValue(State initialState) {
+		return 0.0;
+	}
+	
+	public double transitionProbability(State initialState, State targetState, AgentAction action, TaskDistribution td) {
+		
+		// Chooses the next city according to the current state and action selected
+		//
+		// - If the agent picks up the task, the next city is the destination of the task
+		// - If the agent decides to move, the next city is contained in the move decision
+		//
+		City nextCity = action.isPickup() ? initialState.destinationCity : action.moveCity;
+		
+		
+		// Check if the target state's current city is the same as where the agent is going.
+		if (targetState.currentCity == nextCity) {
+			
+			// Returns probability in this city according to the task distribution parameter.
+			return td.probability(targetState.currentCity, targetState.destinationCity);
+		} else {
+			
+			// Zero chance to be in a different city than the destination if we pick up.
+			return 0.0;
+		}
 	}
 }
